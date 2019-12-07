474a689b95cbb345ad8716231c12d1ec3974d821
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-12-07 17:47:26 +0000

    Try to find libraries

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,10 @@ using ObjectFile, Pango_jll
 @testset "DependencyWalker.jl" begin
     @show Pango_jll.libpango_path
     @show readdir(dirname(Pango_jll.libpango_path))
-    @show dump(readmeta(open(Pango_jll.libpango_path, "r")))
+    open(Pango_jll.libpango_path, "r") do io
+        @show dump(readmeta(io))
+        @show find_libraries(readmeta(io))
+    end
     # @test Library(Pango_jll.libpango_path) isa Library{<:ObjectHandle}
     @test Library("this does not exist.foo") isa Library{Missing}
 end
