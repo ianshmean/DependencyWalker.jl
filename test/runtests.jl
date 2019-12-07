476a5e98b50cefe276f15bbf1c490b2181297a55
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-12-07 17:28:57 +0000

    Show content of the directory where libpango is

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,7 @@ using Test
 using ObjectFile, Pango_jll
 
 @testset "DependencyWalker.jl" begin
+    @show readdir(dirname(Pango_jll.libpango_path))
     @test Library(Pango_jll.libpango_path) isa Library{<:ObjectHandle}
     @test Library("this does not exist.foo") isa Library{Missing}
 end
