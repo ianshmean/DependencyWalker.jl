8d3351e659d5cae5493ac078fc324835e19b036c
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-12-07 17:36:40 +0000

    Dump the libpango object file

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,7 +3,9 @@ using Test
 using ObjectFile, Pango_jll
 
 @testset "DependencyWalker.jl" begin
+    @show Pango_jll.libpango_path
     @show readdir(dirname(Pango_jll.libpango_path))
-    @test Library(Pango_jll.libpango_path) isa Library{<:ObjectHandle}
+    @show dump(readmeta(open(Pango_jll.libpango_path, "r")))
+    # @test Library(Pango_jll.libpango_path) isa Library{<:ObjectHandle}
     @test Library("this does not exist.foo") isa Library{Missing}
 end
