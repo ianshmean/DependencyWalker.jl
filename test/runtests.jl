e12a51cbf1e541c035efde83dcc98bdb06190ec8
Mosè Giordano <m.giordano@ucl.ac.uk>
2019-12-07 18:02:52 +0000

    Force type of the handle

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,9 +5,21 @@ using ObjectFile, Pango_jll
 @testset "DependencyWalker.jl" begin
     @show Pango_jll.libpango_path
     @show readdir(dirname(Pango_jll.libpango_path))
+
+    if Sys.islinux() || Sys.isfreebsd()
+        const handle_type = ELFHandle
+    elseif Sys.isapple()
+        const handle_type = MachOHandle
+    elseif Sys.iswindows()
+        const handle_type = COFFHandle
+    else
+        const handle_type = nothing
+    end
+
     open(Pango_jll.libpango_path, "r") do io
-        @show dump(readmeta(io))
-        @show find_libraries(readmeta(io))
+        oh = readmeta(io, handle_type)
+        @show dump(oh)
+        @show find_libraries(oh)
     end
     # @test Library(Pango_jll.libpango_path) isa Library{<:ObjectHandle}
     @test Library("this does not exist.foo") isa Library{Missing}
